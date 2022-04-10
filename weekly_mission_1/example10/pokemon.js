ee6ad396f96822974bc4b0aa74c86908fa18f343
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-04-09 18:59:27 -0600

    Terminados los ejercicios de la semana 1

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,13 @@
+export class Pokemon {
+    constructor(name){
+        this.name = name
+    }
+    sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda`)
+    }
+    sayMessage(message){
+        console.log(`Mi pokemon ${this.name} dice: ${message}`)
+    }
+}
+
+export default Pokemon
