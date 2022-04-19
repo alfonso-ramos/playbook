da5be39261993344c07bb3acfd6e06417653d17e
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-04-18 21:32:11 -0600

    Terminado el sprint 1: modelos

---
+++ weekly_mission_3/twitter/app/models/User.js
@@ -0,0 +1,30 @@
+class User{
+    constructor(id, username, name, bio, dateCreated, lastUpdate){
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = new Date()
+        this.lastUpdate = new Date()
+    }
+    get getUsername(){
+        return this.username
+    }
+    get getBio(){
+        return this.bio
+    }
+    get getDateCreated(){
+        return this.dateCreated
+    }
+    get getLastUpdated(){
+        return this.lastUpdate
+    }
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+    set setBio(newBio){
+        this.bio = newBio
+    }
+}
+
+module.exports = User
---
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -0,0 +1,28 @@
+const User = require("../../app/models/User")
+
+describe("Unit tests for User class", () => {
+    test("Create an User object", () =>{
+        const user = new User(1, "ponchoramos", "Poncho", "Bio")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("ponchoramos")
+        expect(user.name).toBe("Poncho")
+        expect(user.bio).toBe("Bio")
+        expect(user.dataCreated).not.toBeUndefined
+        expect(user.lastUpdate).not.toBeUndefined()
+    }),
+    test("Add getters", () =>{
+        const user = new User(1, "ponchoramos", "Poncho", "Bio")
+        expect(user.getUsername).toBe("ponchoramos")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    }),
+    test("Add setters", () => {
+        const user = new User(1, "ponchoramos", "Poncho", "Bio")
+        user.setUsername = "Ramos" 
+        expect(user.username).toBe("Ramos")
+
+        user.setBio = "New Bio"
+        expect(user.bio).toBe("New Bio")
+    } )
+})
