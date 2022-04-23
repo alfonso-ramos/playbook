7cafceead7761d57a3256ef899f89f1208b8c807
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-04-23 12:33:45 -0600

    Pendiente de terminar el sprint 3, vistas

---
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -0,0 +1,14 @@
+const UserServices = require("./../services/UserServices")
+
+class UserView{
+    constructor(error){
+        this.error = error
+    }
+    static createUser(payload){
+        if(payload == null) {
+            return 
+        }
+    }
+}
+
+module.exports = UserView
---
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -0,0 +1,25 @@
+const UserView = require("../../app/views/UserView")
+
+describe("Test for UserView", () => {
+    test("Return an error object whenn try to create a new user with a null payload", () => {
+        const payload = null
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/payload no existe/)
+    })
+    /*test("Return an error object when try to create a new user with a payload with invalid properties", () =>{
+        const payload = {username: null, name: 12, id:"id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+    test("Return an error object when try to create a new user with a payload with a missing properties", () =>{
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+    test("Create a user by a given valir payload", () => {
+        const payload = {username:"Username", id:1, name:"name"}
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("Username")
+        expect(result.id).toBe(1)
+    })*/
+})
